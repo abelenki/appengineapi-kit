2a234f30ada1845ab6a6a8624e599e23623f8061
djthorpe <djt@mutablelogic.com>
2013-02-15 09:40:04 +0000

    added files

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 appengineapi-kit
 ================
 
-Tutorial on using Google App Engine to create an API interface in Python
+Introduction
+------------
+
+This is the beginning of a tutorial on using Google App Engine to 
+create an API interface in Python. By "API" I mean a framework which
+allows you to retrieve data from a data store source, manipulate it,
+update, create and delete it. This tutorial will be split into the
+following parts:
+
+  1. The App Engine framework
+  2. Routing API calls
+  3. Data model and data design
+  4. Creating, updating and deleting data
+  5. Using AngularJS to build a web application
+  6. Authentication and authorization for API calls
+  7. Using Google's Cloud SQL service
+
+The App Engine Framework
+------------------------
+
---
+++ application.py
@@ -0,0 +1,35 @@
+#!/opt/local/bin/python2.7
+# encoding: utf-8
+
+__author__ = "djt@mutablelogic.com (David Thorpe)"
+
+# Python imports
+import sys, os
+
+# add python libraries:
+#  - 'app' is for application code
+#  - 'lib' for library modules
+#  - 'lib3' for 3rd party modules
+sys.path.append(os.path.join(os.path.dirname(__file__),'app'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'lib'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'lib3'))
+
+# GAE imports
+import webapp2
+
+# app imports
+from test import apihandler as testapihandler
+
+# CONSTANTS
+DEBUG = True
+
+# route /api/test /api/test/ and /api/test/... messages through to the apihandler
+app = webapp2.WSGIApplication([
+	('/api/test',testapihandler.RequestHandler),
+	('/api/test([\w\/]*)',testapihandler.RequestHandler)
+],debug=DEBUG)
+
+# add values to the registry
+app.registry = {
+	'debug': DEBUG
+}
